6e3542ef43334be168e5e0e658ba44a0ebb25ece
damianbednarz1984 <damianbednarz1984@gmail.com>
2021-05-22 16:12:11 +0100

    UserModelUpdated

--- src/models.py
+++ src/models.py
@@ -1,6 +1,5 @@
 from . import db
 from flask_login import UserMixin
-from sqlalchemy.sql import func
 from datetime import datetime
 
 
@@ -9,6 +8,13 @@ class User(db.Model, UserMixin):
     fname = db.Column(db.String(150))
     lname = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True)
+    phone = db.Column(db.String(100), default="")
+    gender = db.Column(db.String(100), default="")
+    country = db.Column(db.String(100), default="")
+    city = db.Column(db.String(100), default="")
+    zip_code = db.Column(db.String(100), default="")
+    address = db.Column(db.Text, default="")
+    date_of_birth = db.Column(db.String(100), default="")
     password = db.Column(db.Text)
     avatar = db.Column(db.String(225), default="default_user_img.png")
     role = db.Column(db.Boolean(), default=False)
