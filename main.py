0214945b86c9a116a25017db31ec435a99f33d62
Ubuntu <damian@WebApp.24vlcdoxhpiu5iwlbvhkwrrzea.ax.internal.cloudapp.net>
2021-05-30 11:25:05 +0000

    edit views

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from src import create_app
 app = create_app()
 
 if __name__ == "__main__":
-	app.run(debug=True, port=8000)
+	 app.run(host='0.0.0.0',port='443', ssl_context=('../../cert.pem', '../../privkey.pem'))
+
+
